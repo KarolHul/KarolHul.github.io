faa91680016fcf74ede21512b00b6a00103fc565
KarolHul <99616289+KarolHul@users.noreply.github.com>
2024-06-10 18:46:51 +0200

    Languages

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-document.getElementById('languageButton').addEventListener('click', function() {
-    if (document.documentElement.lang === 'pl') {
-        window.location.href = 'index_en.html'; // Przekierowanie do wersji angielskiej
-    } else {
+document.getElementById('plButton').addEventListener('click', function() {
+    if (document.documentElement.lang !== 'pl') {
         window.location.href = 'index.html'; // Przekierowanie do wersji polskiej
+    }
+});
+
+document.getElementById('engButton').addEventListener('click', function() {
+    if (document.documentElement.lang !== 'en') {
+        window.location.href = 'index_en.html'; // Przekierowanie do wersji angielskiej
     }
 });
